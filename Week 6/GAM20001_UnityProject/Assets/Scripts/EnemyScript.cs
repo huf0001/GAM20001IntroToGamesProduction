05c5d470ea443b641a04928cde956e304c7dbfd2
DESKTOP-FJB96GO\SamHuffer <101633177@student.swin.edu.au>
2018-04-12 14:42:27 +1000

    Setting up folder for W6 work

---
+++ Week 6/GAM20001_UnityProject/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,40 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class EnemyScript: MonoBehaviour 
+{
+	private Rigidbody body;
+	public bool randomForce = true;
+	public float velocity = 10f;
+
+    public GameObject collideParticle;
+    private GameObject tempParticles;
+
+    public GameObject controller;
+	private GameplayController gameController;
+	// Use this for initialization
+	void Awake () 
+	{
+		gameController = controller.GetComponent<GameplayController>();
+		body = gameObject.GetComponent<Rigidbody>();
+		body.AddForce(new Vector3(25, 0, 25), ForceMode.Impulse);
+		
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+        if (col.gameObject.tag == "Player")
+        {
+            gameController.RespawnPlayer();
+            Vector3 reflection = body.velocity.normalized + (2 * (Vector3.Dot(body.velocity.normalized, col.contacts[0].normal)) * col.contacts[0].normal);
+            body.velocity = reflection * velocity;
+        }
+
+        else if ((col.gameObject.tag != "Shield") && (collideParticle != null))
+        {
+            tempParticles = (GameObject)Instantiate(collideParticle, transform.position, Quaternion.identity);
+            tempParticles.GetComponent<ParticleSystem>().Play();
+        }
+    }
+
+}
